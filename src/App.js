900661ec628fb9ceafb708c4a14fc637feac7497
Steven <gonghaima@hotmail.com>
2019-11-12 19:33:10 +1100

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import Draggable from './Draggble';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Draggable onDrag={console.log} id="uniqueId">
+        <h2>Drag me</h2>
+      </Draggable>
     </div>
   );
-}
+};
 
 export default App;
